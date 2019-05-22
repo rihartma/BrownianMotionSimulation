18618d07562cb6d1a6ba33fe46d66a504c016678
rihartma <rihartmajr@gmail.com>
2019-05-22 19:07:52 +0200

    added a speed var to the Particle class, new methods for returning and setting the variables

--- particle.h
+++ particle.h
@@ -10,15 +10,37 @@ private:
     int r;
     // mass of the particle
     int m;
+    // speed of the particle
+    float v;
     // position of the particle
     int x;
     int y;
-    // vector representing speed and direction of the particle
+    // unit vector of the particle - representation of the movement direction
     int vx;
     int vy;
 public:
     Particle();
     Particle(int radius, int mass, int x_pos, int y_pos, int vector_x, int vector_y);
+    
+    // working with the position
+    int getX() const { return x; }
+    int getY() const { return y; }
+    void setX(int new_x) { x = new_x; }
+    void setY(int new_y) { y = new_y; }
+
+    // working with the vector
+    int getVx() const { return vx; }
+    int getVy() const { return vy; }
+    void setVx(int new_vx) { vx = new_vx; }
+    void setVy(int new_vy) { vy = new_vy; }
+
+    // working with the velocity
+    float getV() const { return v; }
+    void setV(int new_v) { v = new_v; }
+
+    int radius() const { return r; }  // returns the radius of the particle
+    int mass() const { return m; }  // returns the mass of the particle
+
     ~Particle();
 };
 
